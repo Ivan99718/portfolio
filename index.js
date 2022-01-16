e2c7b50d676b29ca999ddd5dd4ae60b718b6a73c
Ivan99718 <vanya13.93@mail.ru>
2022-01-16 16:21:31 +0300

    fix

---
+++ index.js
@@ -0,0 +1,13 @@
+var name =  num1;
+var name =  num2;
+var name = num3;
+var name = num4;
+var name= num5;
+
+num1 = 10;
+num2 = 20;
+num3 = 48; 
+num4 = 8;
+num5 = 15;
+
+console.log(num1 + num2 + num3 + num4 + num5);
